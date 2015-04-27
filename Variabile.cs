ce0f6f8eba32740a69b51972ce2764ef9e300cff
bbeennyy <costea.beny@gmail.com>
2015-04-27 15:11:52 -0400

    Create Variabile.cs

---
+++ Variabile.cs
@@ -0,0 +1,22 @@
+using UnityEngine;         //astea o sa le ai tot timpu in unity3d
+using System.Collections;  //sunt  librarii unde avem functii ca sa le accesam si de asemenea leaga codul de obiectele din scena
+
+
+
+//declarare variabila
+
+public int variabila;   // public e un fel de a spune ca variabila ta poate fi accesata oricand
+                        // al doilea cuvant e tipul variabilei int =  10 numere naturale sau nnegative fara virgula
+                        //                                      float = 3,5f  sunt cu virgula trebe sa fiu f dupa fiecare numar
+                        //si al 3 cuvant e numele variabilei ,putea fi kakat sau orice
+                        
+public float ani = 10f;
+public bool isCool;   //asta bool e un tip de date care poate fi TRUE sau FALSE ,daca nu declaram nimic e fals
+public bool isNotCool = true; // acum e true
+public bool isNotCoolAtAll = false;
+
+public string nume = "Acesta este un sir de caractere,poti pune orice aici intre ghilimele";
+
+//acum dam valori variabilelor
+variabila = 43;
+
